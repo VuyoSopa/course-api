b86a74573eca0b6f35d0c00ff8ceba052f4685fd
VuyoSopa <vssopangisa@gmail.com>
2023-09-14 12:51:35 +0200

    new updates

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -12,7 +12,7 @@ router.put('/:id', courseController.updatebyObjectId);
 
 //remove by _id: ObjectId(), but before the removing of data us applied data belonging top specified
 // id: is shown and removed at the same time
-//router.delete('/:id', courseController.applyDelete);
+router.delete('/:id', courseController.applyDelete);
 
 router.delete('/remove', courseController.applyRemoveAll);
 
