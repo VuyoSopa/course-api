bdba3be90796dda570df4ce866080a051e2417d5
bigfisssh <eduadvtech@gmail.com>
2023-09-14 12:40:18 +0200

    Please find new updated files

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -3,6 +3,7 @@ const Course = require('../models/course');
 const req = require('express/lib/request');
 const course = require('../models/course');
 
+//creates new course.
 exports.createCourse = async (req, res) => {
     try {
         const course = new Course(req.body);
@@ -14,6 +15,7 @@ exports.createCourse = async (req, res) => {
     }
 };
 
+//when called, it displays pretty much all the data in the database.
 exports.getAllCourses = async (req, res) => {
 try {
     const courses = await Course.find();
@@ -22,3 +24,35 @@ try {
     res.status(500).json({ error: error.message });
 }
 };
+
+//updates info by first specifying ObjectId of the data already captured 
+exports.updatebyObjectId =  async(req, res) => {
+        
+    try {
+        const course =  await Course.findByIdAndUpdate(req.params.id, req.body) 
+
+        res.status(201).json("Changes made successfully");
+      } catch (error) {
+          res.status(500).json({error: error.message})
+    }
+}
+
+//create delete function to remove info admin wishes to remove
+exports.applyDelete = async(req, res) => {
+    try {
+        const result = await Course.findByIdAndRemove(req.params.id)
+        res.status(201).json("Deleted successfully!");
+    } catch (error) {
+        res.status(201).json({error: error.message})
+    }
+}
+
+//removes all the data from the collection
+exports.applyRemoveAll = async(req, res) =>{
+    try {
+        const course = await Course.deleteMany({ }, req.body);
+        res.status(201).json("Removed all data!")
+    } catch (catcherr) {
+        res.status(404).json({ catcherr: catcherr.message })
+    }
+}
--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -5,10 +5,16 @@ const router = express.Router();
 const courseController = require('../controllers/courseController');
 
 router.post('/create', courseController.createCourse);
+
 router.get('/', courseController.getAllCourses);
-// router.get('/:id', courseController.getCourseById);
-// router.put('/:id', courseController.update)
-// router.delete('/:id', courseController.deleteOne)
 
+router.put('/:id', courseController.updatebyObjectId);
 
+//remove by _id: ObjectId(), but before the removing of data us applied data belonging top specified
+// id: is shown and removed at the same time
+//router.delete('/:id', courseController.applyDelete);
+
+router.delete('/remove', courseController.applyRemoveAll);
+
+//translated to module
 module.exports = router;
