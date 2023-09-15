9af393a12b82672dd83bc1dbc59587513c21b44a
VuyoSopa <vssopangisa@gmail.com>
2023-09-15 14:53:36 +0200

    updating schema

--- src/models/course.js
+++ src/models/course.js
@@ -4,6 +4,7 @@ const courseSchema = new mongoose.Schema({
     course: {type: String, required: true},
     modules: {type: String, required: true},
     duration: {type: String, required: true},
+    image: { type: String, required: true },
 });
 
 module.exports = mongoose.model('course', courseSchema);
